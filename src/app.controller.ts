ee9e50b829eae4eb53fb77517a69ae9fcba4eeb2
jkuch612 <jkuch612@gmail.com>
2019-04-15 19:54:50 +0000

    Day 1 done

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/status')
+  getStatus() {
+    return {
+      message: 'everyhting is fine at the server. Thanks for asking',
+      at: new Date().toISOString(),
+    };
+
+  }
+}
