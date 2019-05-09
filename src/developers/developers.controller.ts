9e9a6dff1a9b654ce38dfea62b5f49ef9a3a9a47
jkuch612 <jkuch612@gmail.com>
2019-05-09 19:52:51 +0000

    Lab completed

--- src/developers/developers.controller.ts
+++ src/developers/developers.controller.ts
@@ -10,7 +10,8 @@ export class DevelopersController {
     database: Developer[] = [
         { id: '1', firstName: 'Ben', lastName: 'Hedrick', team: 'boss' },
         { id: '2', firstName: 'Jesse', lastName: 'Taylor', team: 'Quoting' },
-        { id: '3', firstName: 'Zac', lastName: 'Adams', team: 'ERO' }
+        { id: '3', firstName: 'Zac', lastName: 'Adams', team: 'ERO' },
+        { id: '4', firstName: 'John', lastName: 'Kuch', team: 'ERO' },
     ];
 
     @Get()
